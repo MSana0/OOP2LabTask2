b7122be0811608d596f84adde728cf6c00023eff
Marium Sana <65021743+MSana0@users.noreply.github.com>
2020-10-31 12:34:54 +0600

    Add files via upload

--- LabTask/Program.cs
+++ LabTask/Program.cs
@@ -10,23 +10,23 @@ namespace LabTask
     {
         private String name;
         private String id;
-        private String dept;
+        private String department;
         private float cgpa;
 
-        public Student(String Name, String ID, String Dept, float cgpa)
+        public Student(String Name, String ID, String Department, float cgpa)
         {
             name = Name;
             id = ID;
-            dept = Dept;
+            department = Department;
             this.cgpa = cgpa;
         }
 
         public void showinfo()
         {
-            Console.WriteLine("Name: " +name);
-            Console.WriteLine("Id: " +id);
-            Console.WriteLine("Department: " +dept);
-            Console.WriteLine("cgpa: " +cgpa);
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+            Console.WriteLine(department);
+            Console.WriteLine(cgpa);
         }
     }
 
@@ -73,6 +73,59 @@ namespace LabTask
         }
 
     }
+
+    class ACCOUNT
+    {
+        private String accName;
+        private String acid;
+        private int balance;
+
+        public ACCOUNT(String accName, String acid, int balance)
+        {
+            this.accName = accName;
+            this.acid = acid;
+            this.balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            this.balance = balance + amount;
+            Console.WriteLine(balance);
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (balance > amount)
+            {
+                this.balance = balance - amount;
+                Console.WriteLine(balance);
+            }
+
+        }
+    }
+
+    class Course
+    {
+        private String courseName;
+        private String courseCode;
+        private int courseCredit;
+
+        public Course(String courseName, String courseCode, int courseCredit)
+        {
+            this.courseName = courseName;
+            this.courseCode = courseCode;
+            this.courseCredit = courseCredit;
+        }
+
+        public void ShowCourseInfo()
+        {
+            Console.WriteLine(courseName);
+            Console.WriteLine(courseCode);
+            Console.WriteLine(courseCredit);
+        }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
@@ -80,9 +133,16 @@ namespace LabTask
             Student s1 = new Student("Marium", "18-39059-3", "CSE", 3.73F);
             s1.showinfo();
             Console.WriteLine();
-            Triangle t1 = new Triangle(5, 5, 6);
+            Triangle t1 = new Triangle(5, 5, 10);
             t1.showinfo();
             t1.Testtringle();
+            Console.WriteLine();
+            ACCOUNT a1 = new ACCOUNT("Sana", "39059", 10000);
+            a1.Deposit(500);
+            a1.Withdraw(300);
+            Console.WriteLine();
+            Course c1 = new Course("PC", "1011", 3);
+            c1.ShowCourseInfo();
         }
     }
 }
